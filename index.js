b4de5fcada4baade36ca74409c9eff55552d3947
vivek <ranjudevidevi793@gmail.com>
2026-03-30 17:02:18 +0530

    add ci pipeline

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import express from 'express'
 const app = express()
 const PORT = process.env.PORT ?? 8080
 
-app.get('/', (req,res)=>{
-  return res.json({ msg: 'hello from this server '})
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT,()=>{
   console.log('Server is up and running on PORT')
